c14dff67452d65afc105a7d4d924da3080c4424f
Jared Brannan <jared.brannan@wsu.edu>
2021-09-07 13:35:08 -0700

    fix docker commands

--- setup.sh
+++ setup.sh
@@ -9,13 +9,7 @@ docker volume create sparky-vol
 # start container
 if $1
 then
-docker run -d \
-       	--name $1 \
-	--mount source=sparky-vol,target=/notebooks \ 
-	sparky-image 
+docker run -d --name $1 --mount source=sparky-vol,target=/notebooks sparky-image 
 else
-docker run -d \
-	--name spary
-	--mount source=sparky-vol,target=/notebooks \ 
-	sparky-image 
+docker run -d --name sparky --mount source=sparky-vol,target=/notebooks sparky-image 
 fi
