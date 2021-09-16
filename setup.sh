c703502b0caf6df8bd79711c2da9c6f1cba8a7c5
Jared J Brannan <jared.brannan@wsu.edu>
2021-09-16 15:57:46 -0700

    finalize

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@
 docker build -t sparky-image .
 
 # create sparky volume for persistence
-#docker volume create sparky-vol
+docker volume create sparky-vol
 
 # start container
 docker run -it \
 	--mount source=sparky-vol,target=/notebooks \
 	--ip=0.0.0.0 \
-	-p 80:8080 \
+	-p 35565:8080 \
 	-p 2222:22 \
-	-p 35565:8888 \
+	-p 80:8888 \
 	sparky-image
